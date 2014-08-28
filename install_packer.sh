09aa08aa152b615403a5305d673804b4657ac6a9
Chris Kozak <ckozak@gmail.com>
2014-08-28 14:19:29 +0100

    Chris - Limiting output from packer download to keep the logs clean

--- install_packer.sh
+++ install_packer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget https://dl.bintray.com/mitchellh/packer/0.6.1_linux_amd64.zip -O packer.zip
+wget --no-verbose https://dl.bintray.com/mitchellh/packer/0.6.1_linux_amd64.zip -O packer.zip
 unzip packer.zip -d packer
