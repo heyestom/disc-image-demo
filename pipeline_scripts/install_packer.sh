278b708829b07e24848a4b0c2ff6d181b6d78ace
Chris Kozak <ckozak@gmail.com>
2014-09-06 14:03:31 +0100

    Using Snap cache to save Packer for use across builds

--- pipeline_scripts/install_packer.sh
+++ pipeline_scripts/install_packer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-wget --no-verbose https://dl.bintray.com/mitchellh/packer/0.6.1_linux_amd64.zip -O packer.zip
-unzip packer.zip -d packer
+if [ ! -d ${SNAP_CACHE_DIR}/packer ]; then
+    wget --no-verbose https://dl.bintray.com/mitchellh/packer/0.6.1_linux_amd64.zip -O ${SNAP_CACHE_DIR}/packer.zip
+    unzip ${SNAP_CACHE_DIR}/packer.zip -d ${SNAP_CACHE_DIR}/packer
+fi
