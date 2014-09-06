4caaebccfa6416503c47eafa37e4c271025f0cb7
Chris Kozak <ckozak@gmail.com>
2014-09-06 14:14:41 +0100

    Consolidating packer installation into the image assembly script

--- pipeline_scripts/assemble_image.sh
+++ pipeline_scripts/assemble_image.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+#Ensure packer is installed before attempting to build an image
+if [ ! -d ${SNAP_CACHE_DIR}/packer ]; then
+    wget --no-verbose https://dl.bintray.com/mitchellh/packer/0.6.1_linux_amd64.zip -O ${SNAP_CACHE_DIR}/packer.zip
+    unzip ${SNAP_CACHE_DIR}/packer.zip -d ${SNAP_CACHE_DIR}/packer
+fi
+
 ${SNAP_CACHE_DIR}/packer/packer build image_management/digital_ocean_packer_script.json
 
--- pipeline_scripts/install_packer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ ! -d ${SNAP_CACHE_DIR}/packer ]; then
-    wget --no-verbose https://dl.bintray.com/mitchellh/packer/0.6.1_linux_amd64.zip -O ${SNAP_CACHE_DIR}/packer.zip
-    unzip ${SNAP_CACHE_DIR}/packer.zip -d ${SNAP_CACHE_DIR}/packer
-fi
