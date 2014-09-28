3b80f4c254aaaf3f7b5f5c57f869bdbe6358f872
heyestom <heyestom@gmail.com>
2014-09-28 14:55:07 +0100

    removed hacky cache work around :S

--- pipeline_scripts/assemble_image.sh
+++ pipeline_scripts/assemble_image.sh
@@ -3,7 +3,7 @@
 printenv
 
 echo Verifying that Packer has been installed to the application cache...
-if [ ! -d ${SNAP_CACHE_DIR}/packerfda ]; then
+if [ ! -d ${SNAP_CACHE_DIR}/packer ]; then
     echo Packer was missing from the cache. Installing...
     wget --no-verbose https://dl.bintray.com/mitchellh/packer/0.7.1_linux_amd64.zip -O ${SNAP_CACHE_DIR}/packer.zip
     unzip ${SNAP_CACHE_DIR}/packer.zip -d ${SNAP_CACHE_DIR}/packer
