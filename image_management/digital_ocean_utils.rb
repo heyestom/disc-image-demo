0e1523ae578f9896dcde69d25a922851b3c8570a
heyestom <heyestom@gmail.com>
2014-09-28 14:29:13 +0100

    removed return statment that didn't return anything

--- image_management/digital_ocean_utils.rb
+++ image_management/digital_ocean_utils.rb
@@ -38,12 +38,6 @@ class DigitalOceanUtils
     smallest_box_512 = 66
 
     response = @digital_ocean_client.droplets.create({name: name, size_id: smallest_box_512, image_id: image_id, region_id: london, ssh_key_ids: ssh_keys})
-
-    require 'pp'
-    pp response
-    pp response.droplet
-
-    response.droplet.id
   end
 
   def rebuild_droplet_from(droplet_name, image_id)
